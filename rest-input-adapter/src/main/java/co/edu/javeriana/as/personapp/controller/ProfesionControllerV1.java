d441d81ff2b1834f3159dab0b410375d9a9aad7d
IsaRodri05 <misabellara0510@hotmail.com>
2025-06-01 01:05:10 -0500

    Feat: Profession files created in Rest Input adapter carpet

---
+++ rest-input-adapter/src/main/java/co/edu/javeriana/as/personapp/controller/ProfesionControllerV1.java
@@ -0,0 +1,41 @@
+package co.edu.javeriana.as.personapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.javeriana.as.personapp.adapter.ProfesionInputAdapterRest;
+import co.edu.javeriana.as.personapp.model.request.ProfesionRequest;
+import co.edu.javeriana.as.personapp.model.response.ProfesionResponse;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/profesion")
+public class ProfesionControllerV1 {
+
+    @Autowired
+    private ProfesionInputAdapterRest profesionInputAdapterRest;
+    
+    @ResponseBody
+	@GetMapping(path = "/{database}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<ProfesionResponse> profesiones(@PathVariable String database) {
+		log.info("Into personas REST API");
+			return profesionInputAdapterRest.historial(database.toUpperCase());
+	}
+
+    @ResponseBody
+    @PostMapping(path="", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ProfesionResponse crearProfesion(@RequestBody ProfesionRequest request) {
+        log.info("esta en el metodo crearProfesion en el controller del api");
+        return profesionInputAdapterRest.crearProfesion(request);
+    }
+}
